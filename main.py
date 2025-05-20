9fcc3906e5858a5e2332236f8d193bbb31d4f846
Phenya-web <scout.2001@mail.ru>
2025-05-20 17:32:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,27 @@
-from fastapi import FastAPI
-import time
-import logging
 import os
-
 from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
 
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv("API_TOKEN")
+CHANNEL_LINK = os.getenv("CHANNEL_LINK")
 
-WEBHOOK_PATH = f"/bot/{TOKEN}"
-RENDER_WEB_SERVICE_NAME = "<YOUR_RENDER_WEB_SERVICE_NAME>"
-WEBHOOK_URL = "https://" + RENDER_WEB_SERVICE_NAME + ".onrender.com" + WEBHOOK_PATH
-
-logging.basicConfig(filemode='a', level=logging.INFO)
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot=bot)
-
-app = FastAPI()
-
-@app.on_event("startup")
-async def on_startup():
-    webhook_info = await bot.get_webhook_info()
-    if webhook_info.url != WEBHOOK_URL:
-        await bot.set_webhook(
-            url=WEBHOOK_URL
-        )
+dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
-async def start_handler(message: types.Message):
-    user_id = message.from_user.id
-    user_full_name = message.from_user.full_name
-    logging.info(f'Start: {user_id} {user_full_name} {time.asctime()}. Message: {message}')
-    await message.reply(f"Hello, {user_full_name}!")
+async def start(message: types.Message):
+    await message.reply("Привет! Введите номер курса и номер группы (например: 2 31)")
 
 @dp.message_handler()
-async def main_handler(message: types.Message):
+async def handle_input(message: types.Message):
     try:
-        user_id = message.from_user.id
-        user_full_name = message.from_user.full_name
-        logging.info(f'Main: {user_id} {user_full_name} {time.asctime()}. Message: {message}')
-        await message.reply("Hello world!")
+        course, group = message.text.strip().split()
+        if course.isdigit() and group.isdigit():
+            await message.reply(f"Спасибо! Вот ссылка на канал: {CHANNEL_LINK}")
+        else:
+            await message.reply("Ошибка: введите два числа через пробел.")
     except:
-        logging.info(f'Main: {user_id} {user_full_name} {time.asctime()}. Message: {message}. Error in main_handler')
-        await message.reply("Something went wrong...")    
+        await message.reply("Ошибка формата. Пример: 2 31")
 
-@app.post(WEBHOOK_PATH)
-async def bot_webhook(update: dict):
-    telegram_update = types.Update(**update)
-    Dispatcher.set_current(dp)
-    Bot.set_current(bot)
-    await dp.process_update(telegram_update)
-
-@app.on_event("shutdown")
-async def on_shutdown():
-    await bot.get_session().close()
-
-@app.get("/")
-def main_web_handler():
-    return "Everything ok!"
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
